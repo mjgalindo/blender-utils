17692baae3e826f427b4ffbf23342d392a7fc2e2
mjgalindo <mjgalindoramos@gmail.com>
2018-10-21 17:21:45 +0200

    Ignore empty meshes

--- split_all_edges.py
+++ split_all_edges.py
@@ -11,12 +11,12 @@ bpy.ops.wm.open_mainfile(filepath=scene_file)
 bm = bmesh.new()
 meshes = set(o.data for o in bpy.data.objects if o.type == 'MESH')
 for me in meshes:
+    if len(me.polygons) < 1: continue
     if not getattr(me.polygons[0], "use_smooth"):
         bm.from_mesh(me)
         bmesh.ops.split_edges(bm, edges=bm.edges)
         bm.to_mesh(me)
         bm.clear() 
 
-bm.free()
 
 bpy.ops.wm.save_as_mainfile(filepath=scene_file[:-6]+'_split.blend')
