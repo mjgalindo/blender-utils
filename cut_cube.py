e81eea8568b05e62677a141921c142f19473845e
mjgalindo <mjgalindoramos@gmail.com>
2018-11-28 19:46:50 +0100

    Triangulate geometry

--- cut_cube.py
+++ cut_cube.py
@@ -5,6 +5,7 @@ from math import *
 import sys
 import click
 import re
+import bmesh
 
 @click.command()
 @click.argument('scene_file')
@@ -34,9 +35,21 @@ def get_gt(scene_file, center, radius):
 
     # Export what's left
     bpy.ops.object.mode_set(mode='OBJECT')
+    print(bpy.context.scene.objects)
+    for obj in bpy.context.scene.objects:
+        me = obj.data
+        # Get a BMesh representation
+        bm = bmesh.new()
+        bm.from_mesh(me)
+        bmesh.ops.triangulate(bm, faces=bm.faces[:], quad_method=0, ngon_method=0)
+        # Finish up, write the bmesh back to the mesh
+        bm.to_mesh(me)
+        bm.free()
+
     cube_file = scene_file[:scene_file.rfind('.')]+'_cube.ply'
     bpy.ops.export_mesh.ply(filepath=cube_file)
     print(f'|||{cube_file}|||')
+
 
 if __name__ == '__main__':
     sys.argv = sys.argv[sys.argv.index('--'):]
