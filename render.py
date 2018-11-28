288983a18ceaded8dfb0b856b84544ab00e6f90a
mjgalindo <mjgalindoramos@gmail.com>
2018-11-28 12:04:53 +0100

    Adjust positions

--- render.py
+++ render.py
@@ -10,15 +10,24 @@ import click
 @click.argument('output_file')
 def render_scene(scene_file, output_file):
     bpy.ops.wm.open_mainfile(filepath=scene_file)
-    if '/box/' in scene_file:
+    if 'right_wall' in bpy.context.scene.objects and False:
         bpy.context.scene.objects['right_wall'].hide_render = True
         bpy.context.scene.objects['ceiling'].hide_render = True
         bpy.context.scene.objects['back_wall'].hide_render = True
+    if 'wall' in bpy.context.scene.objects:
+        bpy.context.scene.objects['wall'].hide_render = True
+    else:
+        bpy.context.scene.objects['grid_wall'].hide_render = True
+ 
+    bpy.context.scene.objects['hidden_geometry'].active_material.diffuse_color = [1,0,0]
 
-    camera_pos = np.array((2, -1.5, 2)) + (np.array(bpy.context.scene.objects["hidden_geometry"].location) if "hidden_geometry" in bpy.context.scene.objects else 0)
-    camera_rot = [1, 0, 0.75]
+    camera_pos = np.array((0.0, 2, 0)) + (np.array(bpy.context.scene.objects["hidden_geometry"].location) if "hidden_geometry" in bpy.context.scene.objects else 0)
+    camera_pos[2] = 0
+    light_pos = np.array((-0.15, 1, 0.8)) + (np.array(bpy.context.scene.objects["hidden_geometry"].location) if "hidden_geometry" in bpy.context.scene.objects else 0)
+    camera_rot = [pi/2, 0, pi]
+
     # Add a camera fish eye.
-    bpy.context.scene.render.engine = 'CYCLES'
+    #bpy.context.scene.render.engine = 'CYCLES'
     bpy.ops.object.camera_add(view_align=True, location=camera_pos)
     bpy.context.object.rotation_euler[0] = camera_rot[0]
     bpy.context.object.rotation_euler[1] = camera_rot[1]
@@ -28,12 +37,12 @@ def render_scene(scene_file, output_file):
     bpy.context.scene.camera = bpy.context.object
 
     #Add a lamp.
-    bpy.ops.object.lamp_add(type='POINT', view_align=False, location=camera_pos)
+    bpy.ops.object.lamp_add(type='POINT', view_align=False, location=light_pos)
     bpy.context.object.data.cycles.cast_shadow = True
 
     #Set the rendering parameter
-    bpy.context.scene.render.resolution_x = 250
-    bpy.context.scene.render.resolution_y = 250
+    bpy.context.scene.render.resolution_x = 512
+    bpy.context.scene.render.resolution_y = 512
     bpy.context.scene.render.resolution_percentage = 100
     bpy.context.scene.render.pixel_aspect_x = 1
     bpy.context.scene.render.pixel_aspect_y = 1
@@ -45,8 +54,8 @@ def render_scene(scene_file, output_file):
 
     ##sampling;=path tracing 
     bpy.context.scene.cycles.progressive = 'PATH'
-    bpy.context.scene.cycles.samples = 64
-    bpy.context.scene.cycles.max_bounces = 2
+    bpy.context.scene.cycles.samples = 1
+    bpy.context.scene.cycles.max_bounces = 4
     bpy.context.scene.cycles.min_bounces = 1
     bpy.context.scene.cycles.transparent_max_bounces = 1
     bpy.context.scene.cycles.transparent_min_bounces = 1
