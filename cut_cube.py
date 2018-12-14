650f6a74b0f598f4de9eb65c28831a2135f33a38
mjgalindo <mjgalindoramos@gmail.com>
2018-12-14 14:19:08 +0100

    Ignore None objects

--- cut_cube.py
+++ cut_cube.py
@@ -7,13 +7,13 @@ import click
 import re
 import bmesh
 
+
 @click.command()
 @click.argument('scene_file')
-@click.option('--center', default=[0,0,0], nargs=3, type=float)
+@click.option('--center', default=[0, 0, 0], nargs=3, type=float)
 @click.option('--radius', default=1.0, type=float)
 def get_gt(scene_file, center, radius):
     bpy.ops.wm.open_mainfile(filepath=scene_file)
-    bpy.context.scene.world.horizon_color = np.array([148,128,128])/255
 
     # Join the whole scene as a single mesh
     bpy.context.scene.objects.active = bpy.data.objects[0]
@@ -26,23 +26,27 @@ def get_gt(scene_file, center, radius):
     for ax in range(3):
         axis = np.zeros((3,))
         axis[ax] = 1.0
-        for sign in [-1,1]:
+        for sign in [-1, 1]:
             plane_normal = axis * sign
             plane_point = center + plane_normal * radius
             print(radius, plane_point, plane_normal)
             for _ in range(2):
                 bpy.ops.mesh.select_all(action='SELECT')
-                bpy.ops.mesh.bisect(plane_co=plane_point, plane_no=plane_normal, clear_outer=True, threshold=1e-6)
+                bpy.ops.mesh.bisect(
+                    plane_co=plane_point, plane_no=plane_normal, clear_outer=True, threshold=1e-6)
 
     # Export what's left
     bpy.ops.object.mode_set(mode='OBJECT')
     print(bpy.context.scene.objects)
     for obj in bpy.context.scene.objects:
         me = obj.data
+        if me is None:
+            continue
         # Get a BMesh representation
         bm = bmesh.new()
         bm.from_mesh(me)
-        bmesh.ops.triangulate(bm, faces=bm.faces[:], quad_method=0, ngon_method=0)
+        bmesh.ops.triangulate(
+            bm, faces=bm.faces[:], quad_method=0, ngon_method=0)
         # Finish up, write the bmesh back to the mesh
         bm.to_mesh(me)
         bm.free()
