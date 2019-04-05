af1ff63e2755bb7feefb456965f01436783754a5
mjgalindo <mjgalindoramos@gmail.com>
2019-04-05 16:52:57 +0200

    Update colors and reformat

--- render.py
+++ render.py
@@ -5,6 +5,7 @@ from math import *
 import sys
 import click
 
+
 @click.command()
 @click.argument('scene_file')
 @click.argument('output_file')
@@ -18,16 +19,34 @@ def render_scene(scene_file, output_file):
         bpy.context.scene.objects['wall'].hide_render = True
     else:
         bpy.context.scene.objects['grid_wall'].hide_render = True
- 
-    bpy.context.scene.objects['hidden_geometry'].active_material.diffuse_color = [60/255,80/255,220/255]
 
-    camera_pos = np.array((0.0, 2, 0)) + (np.array(bpy.context.scene.objects["hidden_geometry"].location) if "hidden_geometry" in bpy.context.scene.objects else 0)
+    bpy.context.scene.world.horizon_color = [0.0, 0.0, 0.0]
+    bpy.context.scene.world.use_nodes = True
+    bpy.context.scene.world.cycles_visibility.camera = False
+    bpy.context.scene.objects['hidden_geometry'].active_material.diffuse_color = [
+        0.235, 0.314, 0.863]
+    bpy.data.materials['full_lambertian_walls'].diffuse_color = [
+        0.35, 0.35, 0.35]
+    # bpy.context.scene.objects['hidden_geometry'].active_material.diffuse_intensity = 0.9
+    # bpy.context.scene.objects['hidden_geometry'].active_material.specular_color = [220/255,220/255,220/255]
+    # bpy.context.scene.objects['hidden_geometry'].active_material.specular_intensity = 0.3
+    # bpy.context.scene.objects['hidden_geometry'].active_material.specular_shader = "WARDISO"
+    use_hidden_as_base = False
+    base_position = (np.array(bpy.context.scene.objects["hidden_geometry"].location)
+                     if "hidden_geometry" in bpy.context.scene.objects and use_hidden_as_base else np.array([0, -0.25, 0]))
+    near_position = (np.array(bpy.context.scene.objects["hidden_geometry"].location)
+                     if "hidden_geometry" in bpy.context.scene.objects else np.array([0, -0.25, 0]))
+    camera_pos = np.array((0.0, 2, 0)) + base_position
+    render_close_object = True
+    if render_close_object:
+        bpy.context.scene.cycles.film_transparent = True
+        camera_pos[1] = camera_pos[1] - 1.0
     camera_pos[2] = 0
-    light_pos = np.array((-0.15, 1, 0.8)) + (np.array(bpy.context.scene.objects["hidden_geometry"].location) if "hidden_geometry" in bpy.context.scene.objects else 0)
+    light_pos = np.array((-0.15, 1, 0.8)) + near_position
     camera_rot = [pi/2, 0, pi]
 
     # Add a camera fish eye.
-    #bpy.context.scene.render.engine = 'CYCLES'
+    bpy.context.scene.render.engine = 'CYCLES'
     bpy.ops.object.camera_add(view_align=True, location=camera_pos)
     bpy.context.object.rotation_euler[0] = camera_rot[0]
     bpy.context.object.rotation_euler[1] = camera_rot[1]
@@ -36,25 +55,28 @@ def render_scene(scene_file, output_file):
     bpy.context.scene.frame_end = 1
     bpy.context.scene.camera = bpy.context.object
 
-    #Add a lamp.
-    bpy.ops.object.lamp_add(type='POINT', view_align=False, location=light_pos)
+    # Add a lamp.
+    bpy.ops.object.lamp_add(
+        type='POINT', view_align=False, location=light_pos, )
+    print(dir(bpy.data.lamps[-1]), bpy.data.lamps[-1].use_nodes)
+    bpy.data.lamps[-1].node_tree.nodes["Emission"].inputs[1].default_value = 20
     bpy.context.object.data.cycles.cast_shadow = True
 
-    #Set the rendering parameter
+    # Set the rendering parameter
     bpy.context.scene.render.resolution_x = 512
     bpy.context.scene.render.resolution_y = 512
     bpy.context.scene.render.resolution_percentage = 100
     bpy.context.scene.render.pixel_aspect_x = 1
     bpy.context.scene.render.pixel_aspect_y = 1
     bpy.context.scene.render.use_file_extension = True
-    bpy.context.scene.render.image_settings.color_mode ='RGBA'
-    bpy.context.scene.render.image_settings.file_format='PNG' 
+    bpy.context.scene.render.image_settings.color_mode = 'RGBA'
+    bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.context.scene.render.filepath = output_file
     bpy.context.scene.render.image_settings.compression = 90
 
-    ##sampling;=path tracing 
+    # sampling;=path tracing
     bpy.context.scene.cycles.progressive = 'PATH'
-    bpy.context.scene.cycles.samples = 1
+    bpy.context.scene.cycles.samples = 128
     bpy.context.scene.cycles.max_bounces = 4
     bpy.context.scene.cycles.min_bounces = 1
     bpy.context.scene.cycles.transparent_max_bounces = 1
@@ -62,9 +84,11 @@ def render_scene(scene_file, output_file):
     bpy.context.scene.cycles.use_progressive_refine = True
     bpy.context.scene.render.tile_x = 64
     bpy.context.scene.render.tile_y = 64
+    for layer in bpy.context.scene.render.layers:
+        layer.cycles.use_denoising = True
 
-    #Render results
     bpy.ops.render.render(write_still=True)
+
 
 if __name__ == '__main__':
     sys.argv = sys.argv[sys.argv.index('--'):]
